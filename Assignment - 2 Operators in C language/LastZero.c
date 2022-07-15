9355de6b53b955df5a692ed28d35b70c1de32be0
Atul8766 <90234253+Atul8766@users.noreply.github.com>
2022-07-15 19:55:47 +0530

    Update LastZero.c

--- Assignment - 2 Operators in C language/LastZero.c
+++ Assignment - 2 Operators in C language/LastZero.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main(){
-    int num=2345,store=0;
-    num = num/10;
-    printf("Expected output: %d%d\n",num,store);
+    int num=2345;
+    num = num*10;
+    printf("Expected output: %d\n",num);
 }
