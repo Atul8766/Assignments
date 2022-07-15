873b206c93efb03975b170b4cdbc2e846765614f
Atul8766 <90234253+Atul8766@users.noreply.github.com>
2022-07-15 13:32:30 +0530

    Add files via upload

---
+++ Assignment - 3 Decision Control Statements/Three_No.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(){
+    int num1,num2,num3;
+    printf("Enter three number: ");
+    scanf("%d %d %d",&num1,&num2,&num3);
+    if (num1>num2 && num1>num3)
+    {
+        printf("%d is greater\n",num1);
+    }
+    else if (num2>num1 && num2>num3)
+    {
+        printf("%d is greater\n",num2);
+    }
+    else
+    {
+        printf("%d is greater\n",num3);
+    }
+    
+    return 0;
+    
+}
