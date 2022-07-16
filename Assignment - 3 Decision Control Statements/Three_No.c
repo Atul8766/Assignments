1eebd32c44ecf1cccc7c5ede90fc25bd6a4ec8bb
Atul8766 <90234253+Atul8766@users.noreply.github.com>
2022-07-16 12:31:18 +0530

    Update Three_No.c

--- Assignment - 3 Decision Control Statements/Three_No.c
+++ Assignment - 3 Decision Control Statements/Three_No.c
@@ -8,7 +8,7 @@ int main(){
     {
         printf("%d is greater\n",num1);
     }
-    else if (num2>num1 && num2>num3)
+    else if (num2>num3)
     {
         printf("%d is greater\n",num2);
     }
