8a62105affb544dc21e3dfdc6376db0f699f7715
Atul8766 <90234253+Atul8766@users.noreply.github.com>
2022-07-20 19:02:48 +0530

    Add files via upload

---
+++ Assignment 8 Pattern_Problem/9.Pattern.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main()
+{
+    int i, j,k;
+    for (i = 1; i <= 4; i++)
+    {
+        k=1;
+        for (j = 1; j <= 7; j++)
+        {
+            if (j >= i && j<=8-i)
+            {
+                printf("%d",k);
+                j<4?k++:k--;
+            }
+            else
+            {
+                printf(" ");
+            }
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
