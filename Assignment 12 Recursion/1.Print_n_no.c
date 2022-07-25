6618dfa56174dc07910f811a2c5d39e75a81bee6
Atul8766 <90234253+Atul8766@users.noreply.github.com>
2022-07-25 13:44:52 +0530

    Add files via upload

---
+++ Assignment 12 Recursion/1.Print_n_no.c
@@ -0,0 +1,17 @@
+#include <stdio.h> 
+
+void print(int);
+
+int main(){
+   print(10);
+}
+
+void print(int n){
+ if (n>0)
+ {
+    print(n-1);
+    printf("%d ",n);
+ }
+ 
+    
+}
---
+++ Assignment 12 Recursion/6.Print_n_even_rev.c
@@ -0,0 +1,15 @@
+#include <stdio.h> 
+
+void print(int);
+
+int main(){
+   print(10);
+}
+
+void print(int n){
+ if (n>0)
+ {
+    printf("%d ",n*2);
+    print(n-1); 
+ }    
+}
